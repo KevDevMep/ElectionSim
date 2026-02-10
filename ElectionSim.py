4b6c68bdf445fd5c4dff05148c19b998e7d8ad9a
Kevin Dong <lulio.101@outlook.com>
2026-02-10 13:46:53 +0000

    Refactor

--- ElectionSim.py
+++ ElectionSim.py
@@ -2,16 +2,15 @@ import csv
 import random as r
 import matplotlib.pyplot as plt
 import geopandas as gp
+import district
 
-expected = lambda n: min(max((1 + (1 / safe_point) * n) / 2.0, 0.0), 1.0)
-
-def shifter(data, shift_amount, group = ""):
+def shifter(data, shift_amount, safe_point, group = "",):
     demA, demB, totalA, totalB = 0, 0, 0, 0
     for d in data:
         totalA += d.Expected
         if d.Margin > 0:
             demA += 1
-        d.shift(shift_amount, group)
+        d.shift(shift_amount, safe_point, group)
         totalB += d.Expected
         if d.Margin > 0:
             demB += 1
@@ -21,73 +20,6 @@ def shifter(data, shift_amount, group = ""):
         if d.Flipped:
             d.to_string()
             d.flip()
-
-class district:
-    def __init__(self, cd, margin, expected, whitePct, minorityPct, blackPct, hispanicPct, pacificPct, asianPct, nativePct):
-        self.CD = cd
-        self.Margin = margin
-        self.Expected = expected
-        self.Flipped = False
-        self.WhitePct = whitePct
-        self.MinorityPct = minorityPct
-        self.BlackPct = blackPct
-        self.HispanicPct = hispanicPct
-        self.PacificPct = pacificPct
-        self.AsianPct = asianPct
-        self.NativePct = nativePct
-        self.Majority = ""
-
-    def to_string(self):
-        print(f'CD: {self.CD}, Margin: {self.Margin:.2%}, WhitePct: {self.WhitePct:.2%}, MinorityPct: {self.MinorityPct:.2%}, BlackPct: {self.BlackPct:.2%}, HispanicPct: {self.HispanicPct:.2%}, PacificPct: {self.PacificPct:.2%}, NativePct: {self.NativePct:.2%}, Majority: {self.Majority}')
-
-    def to_dict(self):
-        return { 'CD': self.CD, 'Margin': self.Margin,'Expected': self.Expected, 'WhitePct': self.WhitePct, 'MinorityPct': self.MinorityPct, 'BlackPct': self.BlackPct, 'HispanicPct': self.HispanicPct, 'PacificPct': self.PacificPct, 'AsianPct': self.AsianPct, 'NativePct': self.NativePct, 'Majority': self.Majority }
-
-    def shift(self, shift_amount, group = ""):
-        if shift_amount != 0:
-            ajusted = shift_amount
-            match group:
-                case 'W':
-                    ajusted *= self.WhitePct
-                case 'B':
-                    ajusted *= self.BlackPct
-                case 'H':
-                    ajusted *= self.HispanicPct
-                case 'P':
-                    ajusted *= self.PacificPct
-                case 'A':
-                    ajusted *= self.AsianPct
-                case 'N':
-                    ajusted *= self.NativePct
-                case _:
-                    ajusted *= 1
-            if ajusted > 0:
-                if self.Margin < 0 and -self.Margin < ajusted:
-                    self.Flipped = True
-            else:
-                if self.Margin > 0 and -self.Margin > ajusted:
-                    self.Flipped = True
-            self.Margin = max(min((self.Margin + ajusted), 1), -1)
-            self.Expected = expected(self.Margin)
-
-    def flip(self):
-        self.Flipped = not self.Flipped
-
-    def majority(self):
-        if self.WhitePct > .5:
-            self.Majority = "White"
-        elif self.HispanicPct > self.WhitePct and self.HispanicPct > self.BlackPct and self.HispanicPct > self.AsianPct and self.HispanicPct > self.NativePct and self.HispanicPct > self.PacificPct:
-            self.Majority = "Hispanic"
-        elif self.BlackPct > self.WhitePct and self.BlackPct > self.AsianPct and self.BlackPct > self.NativePct and self.BlackPct > self.PacificPct:
-            self.Majority = "Black"
-        elif self.AsianPct > self.WhitePct and self.AsianPct > self.NativePct and self.AsianPct > self.PacificPct:
-            self.Majority = "Asian"
-        elif self.NativePct > self.WhitePct and self.NativePct > self.PacificPct:
-            self.Majority = "Native"
-        elif self.PacificPct > self.WhitePct:
-            self.Majority = "Pacific"
-        else:
-            self.Majority = "Minority"
 
 def stats(data):
     d_stats = {}
@@ -135,8 +67,8 @@ match filetype:
             districtreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
             for row in districtreader:
                 cd = f"{row['State']}-{row['Id']}"
-                d_expected = expected(float(row[dataset]))
-                d = district(cd, float(row[dataset]), d_expected, float(row['WhitePct']), float(row['MinorityPct']), float(row['BlackPct']), float(row['HispanicPct']), float(row['PacificPct']), float(row['AsianPct']), float(row['NativePct']))
+                d = district.District(cd, float(row[dataset]), float(row['WhitePct']), float(row['MinorityPct']), float(row['BlackPct']), float(row['HispanicPct']), float(row['PacificPct']), float(row['AsianPct']), float(row['NativePct']))
+                d.expected(safe_point)
                 d.majority()
                 data.append(d)
     case '1':
@@ -144,18 +76,19 @@ match filetype:
             districtreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
             i = 0
             for row in districtreader:
-                d_expected = expected(float(row['Margin']))
-                d = district(i + 1, float(row['Margin']), d_expected, float(row['WhitePct']), float(row['MinorityPct']), float(row['BlackPct']), float(row['HispanicPct']), float(row['PacificPct']), float(row['AsianPct']), float(row['NativePct']))
+                margin = float(row['DemPct']) - float(row['RepPct'])
+                d = district.District(i + 1, margin, float(row['WhitePct']), float(row['MinorityPct']), float(row['BlackPct']), float(row['HispanicPct']), float(row['PacificPct']), float(row['AsianPct']), float(row['NativePct']))
+                d.expected(safe_point)
                 d.majority()
                 data.append(d)
                 i += 1
     case '2':
-        gdf = gp.read_file(filename)
+        gdf = gp.read_file(filename, use_arrow=True)
         seats = len(gdf)
         for i in range(seats):
             margin = gdf['DemPct'][i] - gdf['RepPct'][i]
-            d_expected = expected(margin)
-            d = district(i + 1, margin, d_expected, gdf['WhitePct'][i], gdf['MinorityPct'][i], gdf['BlackPct'][i], gdf['HispanicPct'][i], gdf['PacificPct'][i], gdf['AsianPct'][i], gdf['NativePct'][i])
+            d = district.District(i + 1, margin, gdf['WhitePct'][i], gdf['MinorityPct'][i], gdf['BlackPct'][i], gdf['HispanicPct'][i], gdf['PacificPct'][i], gdf['AsianPct'][i], gdf['NativePct'][i])
+            d.expected(safe_point)
             d.majority()
             data.append(d)
     case _:
@@ -216,7 +149,7 @@ while True:
                 type = input("Again? ")
         case "1":
             shift_amount = float(input("Shift Amount (As Decmial): "))
-            shifter(data, shift_amount)
+            shifter(data, shift_amount, safe_point)
             type = input("Again? ")
         case "2":
             sd = sorted(data, key=lambda n: n.Margin)
@@ -226,7 +159,7 @@ while True:
         case "3":
             group = input("Racial Group (W: White, B: Black, H: Hispanic, P: Pacific, A: Asian, N: Native): ")
             shift_amount = float(input("Shift Amount (As Decmial): "))
-            shifter(data, shift_amount, group)
+            shifter(data, shift_amount, safe_point, group)
             type = input("Again? ")
         case "4":
             stats(data)
---
+++ district.py
@@ -0,0 +1,70 @@
+expected = lambda n, safe_point: min(max((1 + (1 / safe_point) * n) / 2.0, 0.0), 1.0)
+
+class District:
+    def __init__(self, cd, margin, whitePct, minorityPct, blackPct, hispanicPct, pacificPct, asianPct, nativePct):
+        self.CD = cd
+        self.Margin = margin
+        self.Expected = 0
+        self.Flipped = False
+        self.WhitePct = whitePct
+        self.MinorityPct = minorityPct
+        self.BlackPct = blackPct
+        self.HispanicPct = hispanicPct
+        self.PacificPct = pacificPct
+        self.AsianPct = asianPct
+        self.NativePct = nativePct
+        self.Majority = ""
+
+    def to_string(self):
+        print(f'CD: {self.CD}, Margin: {self.Margin:.2%}, WhitePct: {self.WhitePct:.2%}, MinorityPct: {self.MinorityPct:.2%}, BlackPct: {self.BlackPct:.2%}, HispanicPct: {self.HispanicPct:.2%}, PacificPct: {self.PacificPct:.2%}, NativePct: {self.NativePct:.2%}, Majority: {self.Majority}')
+
+    def to_dict(self):
+        return { 'CD': self.CD, 'Margin': self.Margin,'Expected': self.Expected, 'WhitePct': self.WhitePct, 'MinorityPct': self.MinorityPct, 'BlackPct': self.BlackPct, 'HispanicPct': self.HispanicPct, 'PacificPct': self.PacificPct, 'AsianPct': self.AsianPct, 'NativePct': self.NativePct, 'Majority': self.Majority }
+
+    def shift(self, shift_amount, group = "", safe_point = .15):
+        if shift_amount != 0:
+            ajusted = shift_amount
+            match group:
+                case 'W':
+                    ajusted *= self.WhitePct
+                case 'B':
+                    ajusted *= self.BlackPct
+                case 'H':
+                    ajusted *= self.HispanicPct
+                case 'P':
+                    ajusted *= self.PacificPct
+                case 'A':
+                    ajusted *= self.AsianPct
+                case 'N':
+                    ajusted *= self.NativePct
+                case _:
+                    ajusted *= 1
+            if ajusted > 0:
+                if self.Margin < 0 and -self.Margin < ajusted:
+                    self.Flipped = True
+            else:
+                if self.Margin > 0 and -self.Margin > ajusted:
+                    self.Flipped = True
+            self.Margin = max(min((self.Margin + ajusted), 1), -1)
+            self.expected(safe_point)
+    def flip(self):
+        self.Flipped = not self.Flipped
+
+    def expected(self, safe_point):
+        self.Expected = expected(self.Margin, safe_point)
+
+    def majority(self):
+        if self.WhitePct > .5:
+            self.Majority = "White"
+        elif self.HispanicPct > self.WhitePct and self.HispanicPct > self.BlackPct and self.HispanicPct > self.AsianPct and self.HispanicPct > self.NativePct and self.HispanicPct > self.PacificPct:
+            self.Majority = "Hispanic"
+        elif self.BlackPct > self.WhitePct and self.BlackPct > self.AsianPct and self.BlackPct > self.NativePct and self.BlackPct > self.PacificPct:
+            self.Majority = "Black"
+        elif self.AsianPct > self.WhitePct and self.AsianPct > self.NativePct and self.AsianPct > self.PacificPct:
+            self.Majority = "Asian"
+        elif self.NativePct > self.WhitePct and self.NativePct > self.PacificPct:
+            self.Majority = "Native"
+        elif self.PacificPct > self.WhitePct:
+            self.Majority = "Pacific"
+        else:
+            self.Majority = "Minority"
